9d80df0d86361d73751ea7b0b7348eaec01bd23e
epicseven-cups <jackylin616@gmail.com>
2025-03-13 23:10:34 -0400

    ts

---
+++ phantom_backend/go.mod
@@ -0,0 +1,7 @@
+module github.com/epicseven-cup/phantom/tree/main/phantom_backend
+
+go 1.24.1
+
+require github.com/gorilla/websocket v1.5.3
+
+require github.com/lib/pq v1.10.9 // indirect
--- phantom_backend/main.go
+++ phantom_backend/main.go
@@ -1,1 +1,35 @@
-package phantom_backend
+package main
+
+import (
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
+
+var upgrader = websocket.Upgrader{}
+
+func streamPostIt(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		log.Print("Upgrade: ", err)
+	}
+
+	defer c.Close()
+
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Print("Error when reading: ", err)
+		}
+
+		log.Print("Mesasge Type: ", mt)
+		log.Print("Mesasge: : ", message)
+
+	}
+}
+
+func main() {
+	http.HandleFunc("/postit", streamPostIt)
+	http.ListenAndServe("localhost:3001", nil)
+}
